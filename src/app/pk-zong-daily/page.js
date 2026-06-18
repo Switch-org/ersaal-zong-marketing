b2e808ee865ea65148b578b3f0bf7a23e2043a84
tayyabnazir611 <tayyabnazir611@gmail.com>
2026-06-18 11:11:26 +0500

    added firebase analytics for zong lpt and zong-daily

--- src/app/pk-zong-daily/page.js
+++ src/app/pk-zong-daily/page.js
@@ -1,10 +1,8 @@
-"use client";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import { headers } from "next/headers";
 import crypto from "crypto";
-import { useEffect } from "react";
-import { initAnalytics } from "@/utils/firebase";
+import AnalyticsTracker from "../components/AnalyticsTracker";
 
 const ZongMarketingInputAndOtp = dynamic(
   () => import("../components/ZongMarketingInputAndOtp"),
@@ -65,12 +63,10 @@ export default async function home() {
   console.log("decrypted msisdn is :::", decryptedMsisdn);
   console.log("headers are :::", headersObj);
 
-  useEffect(()=>{
-    initAnalytics()
-  },[])
 
   return (
     <main className="h-[100svh] overflow-hidden font-sans md:h-auto md:overflow-visible">
+      <AnalyticsTracker />
       <section className="mx-auto grid  xs:min-h-screen w-full grid-cols-1 gap-4 overflow-hidden bg-white p-0 shadow-sm xs:relative xs:gap-0 xs:bg-[#d9e4c5] xs:px-3 md:min-h-screen md:h-auto md:grid-cols-[1fr_1.5fr] md:gap-7 md:overflow-visible md:bg-white md:p-4">
         <div className="relative overflow-hidden bg-[#d9e4c5] px-5 xs:bg-transparent xs:px-0 xs:pb-0 xs:pt-0 md:rounded-[22px] md:px-10 md:pb-10 md:pt-12">
           <div className="z-10 flex flex-col items-center justify-center text-center xs:hidden">
